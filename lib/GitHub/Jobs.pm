9bf8ed5a3b88ab8db09b332607336f6c8f42dd88
Ovidiu Nita Tatar <ovn.tatar@gmail.com>
2013-05-13 22:36:44 +0200

    Add module 'Readonly' to Build.PL requires section

--- lib/GitHub/Jobs.pm
+++ lib/GitHub/Jobs.pm
@@ -10,7 +10,7 @@ GitHub::Jobs - interface to the GitHub Jobs API.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
@@ -29,7 +29,7 @@ use HTTP::Request;
 use LWP::UserAgent;
 
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 Readonly my $BASE_URL    => "http://jobs.github.com/positions.json";
 
